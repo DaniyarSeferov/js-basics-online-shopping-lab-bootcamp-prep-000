7d73170758ae689f551e96e8a8ce0f042c2af5b8
Daniyar Seferov <seferovdaniyar@gmail.com>
2018-01-17 12:43:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,20 +11,84 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var max = 100
+ var min = 1
+ var itemPrice = Math.floor(Math.random() * (max - min)) + min
+ var newCartItem = {[item]: itemPrice}
+ cart.push(newCartItem)
+ console.log(`${item} has been added to your cart.`)
+ 
+ return cart
 }
 
 function viewCart() {
   // write your code here
+  var cartLength = cart.length
+  var info = 'In your cart, you have '
+  var item, itemKeys, itemKey, position;
+  
+  if (cartLength) {
+    for (let i=0; i<cartLength; i++) {
+      item = cart[i]
+      itemKeys = Object.keys(item)
+      itemKey = itemKeys[0]
+      position = i + 1
+      
+      if (cartLength == 1) {
+        info += `${itemKey} at $${item[itemKey]}.`
+      }
+      else if (cartLength == 2) {
+        if (position == cartLength) {
+          info += `and ${itemKey} at $${item[itemKey]}.`
+        }
+        else {
+          info += `${itemKey} at $${item[itemKey]} `
+        }
+      }
+      else {
+        if (position == cartLength) {
+          info += `and ${itemKey} at $${item[itemKey]}.`
+        }
+        else {
+          info += `${itemKey} at $${item[itemKey]}, `
+        }
+      }
+    }
+  }
+  else {
+    info = 'Your shopping cart is empty.'
+  }
+  console.log(info) 
 }
 
 function total() {
   // write your code here
+  var cartLength = cart.length
+  var total = 0;
+  var item, itemKeys, itemKey, position;
+  
+  for (let i=0; i<cartLength; i++) {
+    item = cart[i]
+    itemKeys = Object.keys(item)
+    itemKey = itemKeys[0]
+    total += item[itemKey]
+  }
+  return total
 }
 
 function removeFromCart(item) {
   // write your code here
+  var cartLength = cart.length
+  var item, itemKeys, itemKey, position;
+  
 }
 
 function placeOrder(cardNumber) {
   // write your code here
 }
+
+addToCart('lemons');
+addToCart('apples');
+addToCart('potatos');
+viewCart();
+
